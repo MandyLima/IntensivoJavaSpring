a9c271047b0eca74e5f21364585b4066390cf24b
MandyLima <161380181+MandyLima@users.noreply.github.com>
2025-08-24 18:36:48 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinationsIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestinationsIndex() {
+        return destinationsIndex;
+    }
+
+    public void setDestinationsIndex(Integer destinationsIndex) {
+        this.destinationsIndex = destinationsIndex;
+    }
+}
